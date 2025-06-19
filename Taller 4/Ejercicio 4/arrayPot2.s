0ff920e0c0d6ffd5acc61111212fb00a8eb96e87
aguscurii05 <165915720+aguscurii05@users.noreply.github.com>
2025-06-19 13:21:12 -0300

    Add files via upload

--- Taller 4/Ejercicio 4/arrayPot2.s
+++ Taller 4/Ejercicio 4/arrayPot2.s
@@ -40,18 +40,12 @@ fin: j fin
 
 #------------------------------------------
 
-esPot2: 
-
-#tomo el num en a1
-
-addi sp sp -16
-sw ra 0(sp)
-
+esPot2: #tomo el num en a0
 li t0 1 # cargo en t0 2^0
 
 whileP2:
-bgt t0 a1 caso0 #si me paso del num corta y devuelve 0
-beq t0 a1 caso1 #si mi num es pot2 corta 
+bgt t0 a0 caso0 #si me paso del num corta y devuelve 0
+beq t0 a0 caso1 #si mi num es pot2 corta 
 slli t0 t0 1
 j whileP2
 
@@ -65,49 +59,27 @@ li a0 1
 j finP2
 
 finP2:
-lw ra 0(sp)
-addi sp sp 16
 ret
 
 #--------------------------------------------
-potEnArr: #tomo t0=puntero, t1=long
+potEnArr: #tomo a0=puntero, a1=long
+li t0 0           #ac
+li t1 0           #it
+mv t2 a0          #t2
 
-#almaceno en el stack las posiciones que NO quiero modificar
-addi sp, sp -32 
-sw ra, (0)sp
-sw s1, (4)sp
-sw s2, (8)sp
-sw s3, (12)sp
-sw s4, (16)sp
-
-li s1 0           #ac
-li s2 1           #it
-mv s3 t0          #me guardo el puntero
-mv s4 t1          #me guardo la longitud
 whileIA:
-bgt s2 s4 res     #sale del while si recorri todo el array
-lb a1 0(s3)       #guardo en a1 el valor del aray[puntero]
+beq t1 a1 res     #sale del while si recorri todo el array
+lb a0 0(s3)       #guardo en a0 el valor del aray[puntero]
 jal ra esPot2     #chequeo si es potencia de 2
-addi s3 s3 1      #actualizo el puntero
-addi s2 s2 1      #actualizo el iterador
-li t0 1
-bne a0 t0 whileIA #si me devuelve 1 esPot2 lo guardo en s1
-addi s1 s1 1
+addi t2 t2 1      #actualizo el puntero
+addi t1 t1 1      #actualizo el iterador
+addi t0 t0 a0     #le sumo el resultado de esPot2
 j whileIA
 
 res:
     
 #guardo los resultados en a0
-mv a0 s1
+mv a0 t0
 
-#restauro los valores de s1 a s4
-lw s1, (4)sp
-lw s2, (8)sp
-lw s3, (12)sp
-sw s4, (16)sp
-
-#actualizo ra y sp. Luego ret
-lw ra, (0)sp
-addi sp, sp, 32
 ret
 #--------------------------------------------
